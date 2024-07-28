8f1c29f9aab504f1fd10ad1b581608a0d477202f
Rafi Zaman <rafiirfan211@gmail.com>
2024-07-28 16:50:19 +0600

    package delete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9f8266403418464f"; }
+/******/ 		__webpack_require__.h = function() { return "e732a6f972b13cda"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
